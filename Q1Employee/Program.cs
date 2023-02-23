6beb5f72c9fee21316156f0e13015aac239e2c44
Una L <una.lestrange@atu.ie>
2023-02-23 15:11:11 +0000

    updated the framework to 6 and added an employee project

---
+++ Q1Employee/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Q1Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
